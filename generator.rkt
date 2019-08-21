118db2b616fd0a31b8ee860087666b1f9d7feec1
James Liu <jamesliu@au1.ibm.com>
2019-08-21 11:28:48 +1000

    feat: define symbols for cell fills, add board pretty-printing

--- generator.rkt
+++ generator.rkt
@@ -3,7 +3,23 @@
 ;; The number of rows and columns to generate.
 (define n 5)
 
+;; Symbols for different kinds of cells.
+(define tent "⛺️")
+(define tree "🌳")
+(define grass "G")
+(define mud "X")
+
 ;; Create a board of n*n cells, initialised to a default value.
 (define board (for/list ([i n])
-                (for/list ([i n])
-                  "x")))
+                (for/list ([j n])
+                  mud)))
+
+;; Pretty-prints the board with extra spacing between cells.
+(define (pretty-print-board board)
+  (for ([row board])
+    (for ([cell row])
+      (display (format "~a  " cell)))
+    (display "\n\n")))
+
+
+(pretty-print-board board)
